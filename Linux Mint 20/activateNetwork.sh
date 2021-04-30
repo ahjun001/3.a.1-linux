c7527144d1c11ef8dd4eef573fa541f227c376d0
ahjun001 <todo.ubuntu@gmail.com>
2021-04-30 14:07:16 +0800

    added activateNetwork.sh a script to re-activate network after closing expressvpn

---
+++ Linux Mint 20/activateNetwork.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# https://www.thegeekdiary.com/how-to-configure-and-manage-network-connections-using-nmcli/
+# https://www.golinuxcloud.com/nmcli-command-examples-cheatsheet-centos-rhel/
+# https://www.edureka.co/community/66884/activation-of-network-connection-failed-in-linux
+
+case $1 in
+NOC)
+    my_adapter_1=Optimized\ for\ VPN
+    my_adapter_2=Automatic\ DNS
+    my_device=enp1s0
+    ;;
+MSSB)
+    my_adapter_1=Auto\ Linksys06800
+    my_adapter_2="$my_adapter_1"
+    my_device=wlp3s0
+    ;;
+*)
+    echo "first command-line argument $1 not recognized, should be NOC or MSSB"
+    echo 'activateNetwork.sh [NOC, MSSB] [1-6]'
+    exit
+    ;;
+esac
+
+set -x
+case $2 in
+i)
+    inxi -n
+    echo
+    systemctl --type=service | grep -i network
+    echo
+    nmcli general status
+    echo
+    nmcli dev status
+    echo
+    nmcli con show
+    echo
+    echo 'check networking status'
+    nmcli networking
+    echo
+    ;;
+1)
+    nmcli networking on
+    ;;
+2)
+    systemctl restart networking
+    echo $?
+    ;;
+3)
+    systemctl restart NetworkManager
+    echo $?
+    ;;
+4)
+    nmcli connection up "$my_adapter_1"
+    echo $?
+    ;;
+5)
+    nmcli connection up "$my_adapter_2"
+    echo $?
+    ;;
+6)
+    sudo dhclient "$my_device"
+    echo $?
+    ;;
+7)
+    nmcli device connect "$my_device"
+    echo $?
+    ;;
+*)
+    echo 'activateNetwork.sh [NOC, MSSB] [1-6]'
+    ;;
+esac
+set +x
--- Linux Mint 20/matin.sh
+++ Linux Mint 20/matin.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env dash
+
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 sudo youtube-dl -U
