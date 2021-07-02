df0672acce078a6f8f7c876d461557265201a2d9
ahjun001 <todo.ubuntu@gmail.com>
2021-07-02 09:48:26 +0800

    Compacted code presentation

--- Linux Mint 20/matin.sh
+++ Linux Mint 20/matin.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env dash
-echo
-echo
-echo
 set -x
 expressvpn status
-echo
-echo
-echo
+echo; echo; echo
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
-echo
-echo
-echo
+echo; echo; echo
 sudo youtube-dl -U
+echo; echo; echo
 sudo systemctl restart NetworkManager
 set +x
