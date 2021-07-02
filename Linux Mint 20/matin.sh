48b5549f1f24df621b68aad2dd8ad4446ed88043
ahjun001 <todo.ubuntu@gmail.com>
2021-07-02 09:42:55 +0800

    Added check on VPN location

--- Linux Mint 20/matin.sh
+++ Linux Mint 20/matin.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env dash
-echo xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+echo
+echo
+echo
 set -x
+expressvpn status
+echo
+echo
+echo
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
-echo xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+echo
+echo
+echo
 sudo youtube-dl -U
 sudo systemctl restart NetworkManager
 set +x
