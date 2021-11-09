005c75f186c38b7651f7610d753e3796202e2274
ahjun001 <todo.ubuntu@gmail.com>
2021-11-09 18:55:00 +0800

    put yt-dlp update and ,width_reduce.sh in bkgrd

--- Linux Mint 20/,daily.sh
+++ Linux Mint 20/,daily.sh
@@ -3,11 +3,12 @@
 clear
 set -x
 expressvpn status
+sudo yt-dlp -U &
+echo; echo; echo
+,width_reduce.sh &
 echo; echo; echo
 sudo apt update -y && sudo apt upgrade -y
 # sudo apt autoremove -y
-echo; echo; echo
-sudo yt-dlp -U
-# echo; echo; echo
 # sudo systemctl restart NetworkManager
+echo; echo; echo
 set +x
