a9e5edf17b87a155331755c914c666d4df86a2dc
ahjun001 <todo.ubuntu@gmail.com>
2021-11-29 17:24:21 +0800

    correct typo on directory

--- Linux Mint 20/reset all links.sh
+++ Linux Mint 20/reset all links.sh
@@ -29,7 +29,7 @@ ln -fs '/home/perubu/Documents/'"${GitHubDir}"'/1.4-learning-vim/vimrc' '/home/p
 ln -fs '/home/perubu/Documents/'"${GitHubDir}"'/3.a.1-Linux/3. zsh/.zshrc' '/home/perubu/'
 
 # link to all Github bash scripts whose filename starts with ,
-find /home/perubu/Documents/GitHub_NOC/3.a.1-Linux/Linux\ Mint\ 20/ -type f -name ',*.sh' -exec sudo ln -fs {} /usr/local/sbin/ \;
+find /home/perubu/Documents/GitHub_NOC/3.a.1-linux/Linux\ Mint\ 20/ -type f -name ',*.sh' -exec sudo ln -fs {} /usr/local/sbin/ \;
 # where the literal {} gets substituted by the filename and the literal \; is needed for find to know that the custom command ends there.
 
 set +x
