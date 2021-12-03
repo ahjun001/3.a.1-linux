d6bcd3f9106666a7f8d545705a50983f6625d41f
ahjun001 <todo.ubuntu@gmail.com>
2021-12-03 19:21:25 +0800

    fix bug on address

--- Linux Mint 20/reset all links.sh
+++ Linux Mint 20/reset all links.sh
@@ -26,10 +26,12 @@ set -x
 ln -fs '/home/perubu/Documents/'"${GitHubDir}"'/1.4-learning-vim/vimrc' '/home/perubu/.vim/'
 
 # link to Github .zshrc
-ln -fs '/home/perubu/Documents/'"${GitHubDir}"'/3.a.1-Linux/3. zsh/.zshrc' '/home/perubu/'
+ln -fs '/home/perubu/Documents/'"${GitHubDir}"'/3.a.1-linux/3. zsh/.zshrc' '/home/perubu/'
 
 # link to all Github bash scripts whose filename starts with ,
 find /home/perubu/Documents/GitHub_NOC/3.a.1-linux/Linux\ Mint\ 20/ -type f -name ',*.sh' -exec sudo ln -fs {} /usr/local/sbin/ \;
 # where the literal {} gets substituted by the filename and the literal \; is needed for find to know that the custom command ends there.
 
+# link to VSCode snippets
+find /home/perubu/Documents/GitHub_NOC/3.a.2-vsCode/config/Code/User/snippets/ -type f -name ',*.json' -exec sudo ln -fs {} /home/perubu/.config/Code/User/snippets/ \;
 set +x
