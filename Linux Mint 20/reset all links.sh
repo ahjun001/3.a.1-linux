e48ad6b4c5a36749259d11dabe28fd70d6c5300d
ahjun001 <todo.ubuntu@gmail.com>
2021-12-03 19:27:15 +0800

    fix bug on address

--- Linux Mint 20/reset all links.sh
+++ Linux Mint 20/reset all links.sh
@@ -29,9 +29,9 @@ ln -fs '/home/perubu/Documents/'"${GitHubDir}"'/1.4-learning-vim/vimrc' '/home/p
 ln -fs '/home/perubu/Documents/'"${GitHubDir}"'/3.a.1-linux/3. zsh/.zshrc' '/home/perubu/'
 
 # link to all Github bash scripts whose filename starts with ,
-find /home/perubu/Documents/GitHub_NOC/3.a.1-linux/Linux\ Mint\ 20/ -type f -name ',*.sh' -exec sudo ln -fs {} /usr/local/sbin/ \;
+find /home/perubu/Documents/${GitHubDir}/3.a.1-linux/Linux\ Mint\ 20/ -type f -name ',*.sh' -exec sudo ln -fs {} /usr/local/sbin/ \;
 # where the literal {} gets substituted by the filename and the literal \; is needed for find to know that the custom command ends there.
 
 # link to VSCode snippets
-find /home/perubu/Documents/GitHub_NOC/3.a.2-vsCode/config/Code/User/snippets/ -type f -name ',*.json' -exec sudo ln -fs {} /home/perubu/.config/Code/User/snippets/ \;
+find /home/perubu/Documents/${GitHubDir}/3.a.2-vsCode/config/Code/User/snippets/ -type f -name ',*.json' -exec sudo ln -fs {} /home/perubu/.config/Code/User/snippets/ \;
 set +x
