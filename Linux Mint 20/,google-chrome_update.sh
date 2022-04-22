50586a588f71d0df708692a7572b99b549506007
ahjun001 <todo.ubuntu@gmail.com>
2022-04-23 06:42:22 +0800

    reorg dirs

--- Linux Mint 20/,google-chrome_update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-clear
-set -x
-wget -P /tmp/ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install /tmp/google-chrome-stable_current_amd64.deb
-set +x
--- Linux Mint 20/pj_restart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -x
-sudo service network-manager restart
-echo $?
-set +x
