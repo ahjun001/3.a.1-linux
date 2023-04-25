7a74acc0e08acfe6bcd5711eb8af07400f0cb17a
ahjun001 <todo.ubuntu@gmail.com>
2023-04-25 18:56:47 +0800

    msg mods

--- 2. bash/getopts/getopts-help-debug-n-test/tests/suites/others/test_dbg.sh
+++ 2. bash/getopts/getopts-help-debug-n-test/tests/suites/others/test_dbg.sh
@@ -2,7 +2,7 @@
 
 echo -n 'Testing debugging'
 
-# Exit 0
+# expecting exit 0
 ../getopts-help-debug-n-test.sh -d >/dev/null || exit 1
 var=$(../getopts-help-debug-n-test.sh -d) && [[ ${var:0:24} == $'flag d\nTesting debugging' ]] || exit 1
 
--- 2. bash/getopts/getopts-help-debug-n-test/tests/suites/usage/test_usage.sh
+++ 2. bash/getopts/getopts-help-debug-n-test/tests/suites/usage/test_usage.sh
@@ -2,11 +2,11 @@
 
 echo -n 'Testing usage'
 
-# exit 0
+# expecting exit 0
 ../getopts-help-debug-n-test.sh -h >/dev/null || exit 1
 var=$(../getopts-help-debug-n-test.sh -h) && [[ ${var:0:6} == $'Usage:' ]] || exit 1
 
-# exit 1
+# expecting exit 1
 ../getopts-help-debug-n-test.sh -x >/dev/null && exit 1
 [[ $(../getopts-help-debug-n-test.sh -x) == 'flag is not managed' ]] || exit 1
 
